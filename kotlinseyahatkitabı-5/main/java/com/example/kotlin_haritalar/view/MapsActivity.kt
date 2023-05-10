7c90c69a3629feb980328c284c3b597ee4582c9f
Emirhanyungul <115731130+Emirhanyungul@users.noreply.github.com>
2023-05-10 13:37:50 +0300

    kotlin RXjava eklendi
    kotlin Rxjava kullanılan uygulamada eğer kullanıcı çok fazla data kayd ederse uygulamanın da çökmemesi için fazladan kullanılmayan dataları telefondan silen bir java kodudur bu kodu kotlin kullandım çünkü java kodları kotlin de çalışmaktadır rxjavayı dao interface te compatable ve flowable kodları ile  oluşturdum . bir private handleresponse fonksiyonu olusturdum bunun sebebi bir data telefona kayd edildiğinde tekrardan uygulamanın ana sayfaya dönmesi anasayfaya dönen uygulama önceki sayfayı kapatması için FLAG komutu eklendi uygulamanın arkadan bir sayfa daha çalıtırması uygulamayı yavaşlatması engellenmiş oldu

---
+++ kotlinseyahatkitabı-5/main/java/com/example/kotlin_haritalar/view/MapsActivity.kt
@@ -0,0 +1,154 @@
+package com.example.kotlin_haritalar.view
+
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.room.Room
+import com.example.kotlin_haritalar.R
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.kotlin_haritalar.databinding.ActivityMapsBinding
+import com.example.kotlin_haritalar.rooms.daoInterface
+import com.example.kotlin_haritalar.rooms.yerDatabase
+import com.example.kotlin_haritalar.yer
+import com.google.android.material.snackbar.Snackbar
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMapLongClickListener {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
+    private  lateinit var permissionlauncher: ActivityResultLauncher<String>
+    private lateinit var  sharedPreferences: SharedPreferences
+    private var trackBoolean : Boolean? = null
+    private var selectedlongitude :Double?=null
+    private var selectedlatitude :Double?=null
+    private lateinit var db:yerDatabase
+    private lateinit var placedao: daoInterface
+    val compositeDisposable = CompositeDisposable()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        registerLauncher()
+        sharedPreferences=this.getSharedPreferences("com.example.kotlin_haritalar", MODE_PRIVATE)
+        trackBoolean = false
+        selectedlongitude=0.0
+        selectedlatitude=0.0
+
+        db = Room.databaseBuilder(applicationContext,yerDatabase::class.java,"Place")
+            //.allowMainThreadQueries()
+            .build()
+        placedao = db.YerDao()
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.setOnMapLongClickListener(this)
+
+        locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
+        locationListener= object :LocationListener {
+            override fun onLocationChanged(p0: Location) {
+                trackBoolean = sharedPreferences.getBoolean("trackboolean",false)
+                if (trackBoolean==false){
+                    val userLocation = LatLng(p0.latitude,p0.longitude)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15f))
+                    sharedPreferences.edit().putBoolean("trackboolean",true).apply()
+                }
+
+            }
+        }
+
+        if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.ACCESS_FINE_LOCATION)){
+                Snackbar.make(binding.root,"permission needed to run this app",Snackbar.LENGTH_INDEFINITE).setAction("give permission"){
+                        permissionlauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                }.show()
+            }else{
+                permissionlauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+            val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if(lastLocation!=null){
+                val lastUserLocation= LatLng(lastLocation.latitude,lastLocation.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15f))
+            }
+            mMap.isMyLocationEnabled = true
+        }
+
+    }
+    private fun registerLauncher(){
+        permissionlauncher=registerForActivityResult(ActivityResultContracts.RequestPermission()){result->
+            if(result){
+                if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+                    val lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                    if(lastLocation!=null){
+                        val lastUserLocation= LatLng(lastLocation.latitude,lastLocation.longitude)
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15f))
+                    }
+                }
+
+            }else{
+                Toast.makeText(this@MapsActivity,"permission is needed",Toast.LENGTH_LONG).show()
+            }
+
+        }
+    }
+
+    override fun onMapLongClick(p0: LatLng) {
+        mMap.clear()
+        mMap.addMarker(MarkerOptions().position(p0))
+        selectedlatitude =p0.latitude
+        selectedlongitude=p0.longitude
+    }
+
+    fun save(view : View){
+        if(selectedlatitude != null && selectedlongitude != null) {
+            val place = yer(binding.placeText.text.toString(), selectedlatitude!!, selectedlongitude!!)
+            compositeDisposable.add(
+                placedao.insert(place).subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread()).subscribe(this::handleresponse)
+            )
+        }
+    }
+
+    private fun handleresponse(){
+        val intent = Intent(this,MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+    }
+    fun delete(view: View){
+
+    }
+}
